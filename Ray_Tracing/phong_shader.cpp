04424899655dc496bdda7ccdb358e476dddea87a
Xiaotian Ma <mxt6232996077@gmail.com>
2020-01-25 17:37:48 -0800

    Added phong_shader and sphere normal
    Added phong shader
    Added the normal function of sphere

--- Ray_Tracing/phong_shader.cpp
+++ Ray_Tracing/phong_shader.cpp
@@ -3,12 +3,29 @@
 #include "ray.h"
 #include "render_world.h"
 #include "object.h"
+#include <algorithm>
 
 vec3 Phong_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
-    vec3 color;
-    TODO; //determine the color
+    vec3 color(0,0,0);
+    
+    Ray shadowRay;
+    Light* currLight;
+    vec3 lightIntensity;
+    for(unsigned i = 0; i < world.lights.size(); i++) {
+        //draw shadow ray to each light source
+        currLight = world.lights.at(i);
+        shadowRay.direction = (currLight->position - intersection_point).normalized();
+        shadowRay.endpoint = currLight->position;
+        
+        //compute diffuse color
+        double cosAngle = std::max(0.0,dot(normal,shadowRay.direction));
+        //amount of light accourding to the distance from the light
+        lightIntensity = currLight->Emitted_Light(currLight->position - intersection_point);
+        color += lightIntensity * color_diffuse * cosAngle;
+    }
+    
     return color;
 }
--- Ray_Tracing/sphere.cpp
+++ Ray_Tracing/sphere.cpp
@@ -43,7 +43,7 @@ Hit Sphere::Intersection(const Ray& ray, int part) const
 vec3 Sphere::Normal(const vec3& point, int part) const
 {
     vec3 normal;
-    TODO; // compute the normal direction
+    normal = (point - center) / radius;
     return normal;
 }
 
