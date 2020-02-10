4bfee6ccc43ac99ca3c47ceff23c878fa458fb2b
Xiaotian Ma <mxt6232996077@gmail.com>
2020-02-10 11:25:53 -0800

    Update reflective shader

--- Ray_Tracing/reflective_shader.cpp
+++ Ray_Tracing/reflective_shader.cpp
@@ -7,6 +7,15 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
     vec3 color;
-    TODO; // determine the color
+    
+    Ray reflectRay;
+    
+    reflectRay.endpoint = intersection_point;
+    reflectRay.direction = (ray.direction - 2 * dot(ray.direction,normal) * normal).normalized();
+    
+    vec3 reflectColor = world.Cast_Ray(reflectRay, recursion_depth+1);
+    
+    color = (1-reflectivity) * shader->Shade_Surface(ray,intersection_point,normal,recursion_depth) + reflectivity * reflectColor;
+    
     return color;
 }
